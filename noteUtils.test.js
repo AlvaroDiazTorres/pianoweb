a95752b071e92d248bf4e4883cdcf4a21f264418
AlvaroDiazTorres <adtclase@gmail.com>
2024-09-18 13:09:38 +0200

    Updates

--- noteUtils.test.js
+++ noteUtils.test.js
@@ -1,7 +1,26 @@
 import { expect, test } from 'vitest'
 import getNoteFromKey from '../noteUtils'
 
-test('Si le paso la letra a devolverá C4', ()=>{
+test('Si le paso la letra a devolverá C4', () => {
     let note = getNoteFromKey('a')
     expect(note).toBe('C4')
+})
+
+test('Si le paso la letra A mayúscula devolverá C4', () => {
+    let note = getNoteFromKey('A')
+    expect(note).toBe('C4')
+})
+
+test('Tecla null', () => {
+    let note = getNoteFromKey(null)
+    expect(note).toBeUndefined
+})
+
+test('No pasas ninguna tecla', () => {
+    let note = getNoteFromKey()
+    expect(note).toBeUndefined
+})
+test('Tecla undefined', () => {
+    let note = getNoteFromKey(undefined)
+    expect(note).toBeUndefined
 })
